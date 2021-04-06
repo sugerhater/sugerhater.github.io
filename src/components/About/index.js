cbc797c28fc6621a201765ff59d0f333bfb5be50
sugerhater <kehong0926@gmail.com>
2021-04-05 23:41:10 -0400

    update about.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () =>{
       {/* <p className="margin-x auto">
        Hi, I am Kehong Li. I was born and grew up in a small village in the middle of China. Because of complicated reasons, after graduated from high school in 2011, I determined to come to America to peruse freedom and eventually land on US soil in 2013 as a student. Because I love America and wanted to be a part of our country, in 2016 I enlisted for the US army reserve to become an American. I graduated from OSU in 2017 and was kept waiting for the army to send me to basic training and my citizenship. Finally, after a long and painful waiting period, I was naturalized on Sep 26th, 2019.
       </p> */}
-      <p className="margin-x auto">
+      <p id = "one" className="margin-x auto">
        Hi, I am Kehong Li. I am from a small village in the middle of China.
        I came to the U.S. in 2013 as a student and enlisted in the Army Reserve a few years later.
        I had the privilege to become a naturalized citizen on Sep 26th,
@@ -18,8 +18,8 @@ const About = () =>{
       {/* <p >
       After I became a US citizen, I worked for Team Fishel for 6 months and then decided to enroll in an OSU full stack web developer boot camp to start my software developer career. I think I am a really good student in the boot camp and ready for a web developing job.
       </p> */}
-      <p>
-      After I became a US citizen, I became a member of Team Fishel for 6 months before enrolling in The 
+      <p id = "two">
+      After the naturalization ceremony, I worked Team Fishel for 6 months before enrolling in The 
       Ohio State Full-Stack Web Developer Boot Camp to start my software developer career. I developed my skills in the bootcamp and am ready for developing the web.
       </p>
       <p id="fact">
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,7 +10,7 @@ const Navbar = () => {
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav mr-auto mt-1 mt-lg-0">
-          <a class="nav-link" href="/react-portfolio">Home</a>
+          <a class="nav-link" href="/">Home</a>
           <a class="nav-link" href="#about">About</a>
           <a class="nav-link" href="#portfolio">Porfolio</a>
           <a class="nav-link" href="#contact">Contact</a>
